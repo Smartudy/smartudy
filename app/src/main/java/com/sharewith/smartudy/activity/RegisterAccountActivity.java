b4758ad24d3a304d40ca9fdd75516d291e49bf8d
chebaum <chebaum@naver.com>
2018-07-20 19:16:17 +0900

    무의미한 커밋 - 이제 EditText 키보드 엔터(Enter)키 기능 변경 해야함(참고:http://ccdev.tistory.com/31)

--- app/src/main/java/com/sharewith/smartudy/activity/RegisterAccountActivity.java
+++ app/src/main/java/com/sharewith/smartudy/activity/RegisterAccountActivity.java
@@ -9,6 +9,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -27,7 +28,7 @@ public class RegisterAccountActivity extends AppCompatActivity {
     private boolean isNickNameValid = false;
     ArrayList<String> major_list;
 
-    String nickname ="";
+    String nickname = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,7 +43,7 @@ public class RegisterAccountActivity extends AppCompatActivity {
 
         updateMajorListFromDB();
 
-        final EditText editText = ((EditText)findViewById(R.id.textview_final_authentication_auth_digit));
+        final EditText editText = ((EditText) findViewById(R.id.textview_final_authentication_auth_digit));
 //        editText.setOnTouchListener(new View.OnTouchListener() {
 //            @Override
 //            public boolean onTouch(View v, MotionEvent event) {
@@ -54,24 +55,22 @@ public class RegisterAccountActivity extends AppCompatActivity {
         editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) { // TODO: loophole 없는지 확인하자!
-                if(!hasFocus){
+                if (!hasFocus) {
                     // TODO: if()문의 조건을 바꿔야함 - if(이미 존재하는 아이디인 경우){} / else{}
                     String userInput = editText.getText().toString();
-                    if(isNickNameValid && userInput.equals(nickname)) return;
+                    if (isNickNameValid && userInput.equals(nickname)) return;
 
-                    if (userInput.equals("공학수학마스터")){
-                        isNickNameValid=false;
+                    if (userInput.equals("공학수학마스터")) {
+                        isNickNameValid = false;
                         nickname = "";
                         editText.setBackgroundResource(R.drawable.edittext_round_white_background_radius_6dp_red_border);
                         Toast.makeText(getApplicationContext(), "이미 존재하는 닉네임 입니다.", Toast.LENGTH_LONG).show();
-                    }
-                    else if(!isValid(userInput)){
-                        isNickNameValid=false;
+                    } else if (!isValid(userInput)) {
+                        isNickNameValid = false;
                         editText.setBackgroundResource(R.drawable.edittext_round_white_background_radius_6dp_red_border);
                         Toast.makeText(getApplicationContext(), "형식에 맞는 닉네임이 아닙니다.", Toast.LENGTH_LONG).show();
-                    }
-                    else{
-                        isNickNameValid=true;
+                    } else {
+                        isNickNameValid = true;
                         nickname = userInput;
                         editText.setBackgroundResource(R.drawable.edittext_round_white_background_radius_6dp_blue_border);
                         Toast.makeText(getApplicationContext(), "사용 가능한 닉네임 입니다.", Toast.LENGTH_LONG).show();
@@ -79,12 +78,12 @@ public class RegisterAccountActivity extends AppCompatActivity {
                 }
             }
         });
-        final Button btn_major = ((Button)findViewById(R.id.btn_register_account_select_major));
+        final Button btn_major = ((Button) findViewById(R.id.btn_register_account_select_major));
         btn_major.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterAccountActivity.this);
-                String[] majors=new String[major_list.size()];
+                String[] majors = new String[major_list.size()];
                 major_list.toArray(majors);
                 builder.setItems(majors, new DialogInterface.OnClickListener() {
                     @Override
@@ -97,12 +96,12 @@ public class RegisterAccountActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
-        final Button btn_grade = ((Button)findViewById(R.id.btn_register_account_select_grade));
+        final Button btn_grade = ((Button) findViewById(R.id.btn_register_account_select_grade));
         btn_grade.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(RegisterAccountActivity.this);
-                final String[] grade={"1","2","3","4","5"};
+                final String[] grade = {"1", "2", "3", "4", "5"};
                 builder.setItems(grade, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
@@ -114,26 +113,26 @@ public class RegisterAccountActivity extends AppCompatActivity {
                 dialog.show();
             }
         });
-        ((Button)findViewById(R.id.btn_register_account_register)).setOnClickListener(new View.OnClickListener() {
+        ((Button) findViewById(R.id.btn_register_account_register)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO: 현재까지 입력한 정보를 바탕으로 회원등록, 로그인 창으로 다시 돌아간다.
-                if(isNickNameValid) { // TODO: 다른조건도 확인해야한다! 학과 및 학년까지 모두 설정했는지.
+                if (isNickNameValid) { // TODO: 다른조건도 확인해야한다! 학과 및 학년까지 모두 설정했는지.
                     // addAccountToDB();
                     finish();
                 }
-                else{
+                else {
                     Toast.makeText(RegisterAccountActivity.this, "정보입력이 불완전합니다.", Toast.LENGTH_SHORT).show();
                 }
             }
         });
     }
 
-    public void updateMajorListFromDB(){
+    public void updateMajorListFromDB() {
         major_list = new ArrayList<String>();
         // TODO: update major list from the database
-        String[] majors = {"가정교육과","가정학과","간호학과","건강관리학과","건축공학과","건축학과","게임공학과","경영정보학과","경영학과"};
-        for(String str: majors)
+        String[] majors = {"가정교육과", "가정학과", "간호학과", "건강관리학과", "건축공학과", "건축학과", "게임공학과", "경영정보학과", "경영학과"};
+        for (String str : majors)
             major_list.add(str);
     }
 
@@ -147,10 +146,10 @@ public class RegisterAccountActivity extends AppCompatActivity {
     public boolean dispatchTouchEvent(MotionEvent event) {
         if (event.getAction() == MotionEvent.ACTION_DOWN) {
             View v = getCurrentFocus();
-            if ( v instanceof EditText) {
+            if (v instanceof EditText) {
                 Rect outRect = new Rect();
                 v.getGlobalVisibleRect(outRect);
-                if (!outRect.contains((int)event.getRawX(), (int)event.getRawY())) {
+                if (!outRect.contains((int) event.getRawX(), (int) event.getRawY())) {
                     //((EditText) v).setCursorVisible(false);
                     //v.setFocusable(false);
                     v.clearFocus();
@@ -159,13 +158,12 @@ public class RegisterAccountActivity extends AppCompatActivity {
                 }
             }
         }
-        return super.dispatchTouchEvent( event );
+        return super.dispatchTouchEvent(event);
     }
     //isValid(userInput)
 
-    public boolean isValid(String str){
-        if(str.length()<1 || str.length()>20) return false;
+    public boolean isValid(String str) {
+        if (str.length() < 1 || str.length() > 20) return false;
         else return true;
     }
-
 }
